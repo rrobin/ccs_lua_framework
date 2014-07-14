e208efbabafc20aef0d35f0a5dca24bba47379b6
rrobin <102623012@qq.com>
2014-07-14 19:58:50 +0800

    添加display模块函数
    -支持对jpg+I8 mask这样的组合方式创建sprite
    -增加了一个sprite自动根据文件名来创建对象的函数:AutoCreate(filename)

--- frameworks/runtime-src/Classes/cocosEx/FileIO.h
+++ frameworks/runtime-src/Classes/cocosEx/FileIO.h
@@ -9,6 +9,7 @@ public:
 	CREATE_FUNC(FileIO);
 	virtual bool init(){return true;}
 	ByteBuffer* getFileData(const char* filename);
+	Texture2D* openImage(const char* jpgFile,const char* maskFile);
 private:
 	ByteBuffer readBuffer;
 };
--- src/framework/display/SpriteEx.lua
+++ src/framework/display/SpriteEx.lua
@@ -9,3 +9,54 @@ function Sprite:setShader(shaderName)
         self:setDirty(true)
     end
 end
+
+function Sprite:AutoCreate(filename)
+
+	local tag = -1
+	local texture = nil
+	while true
+	do
+		local Director = c.Director:getInstance()
+		texture = Director:getTextureCache():getTextureForKey(filename)
+		if texture then
+			tag = 1
+			break
+		end
+		-- find ".png"
+		local s,e = string.find(filename,".png")
+		if s then
+			tag = 0
+			break
+		end
+
+		-- find ".pvr"
+		s,e = string.find(filename,".pvr")
+		if s then
+			tag = 0
+			break
+		end
+
+		-- find ".jpg"
+		s,e = string.find(filename,".jpg")
+		if s then
+			local maskfile = string.gsub(filename,".jpg",".mask")
+			if c.isFileExist(maskfile) then
+				texture = cc.openImage(filename,maskfile)
+				tag = 1
+				break
+			else
+				tag = 0
+				break
+			end
+		end
+
+		-- without extension
+		filename = filename..".jpg"
+	end
+
+	if tag == 0 then
+		return Sprite:create(filename)
+	elseif tag == 1 then
+		return Sprite:createWithTexture(texture)
+	end	
+end
